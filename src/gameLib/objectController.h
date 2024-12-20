9c5e3bb36b57b4578b205e4c94f2d7549ab3c27a
aznoH333 <glentik23@gmail.com>
2024-12-20 10:14:19 +0100

    thing

--- src/gameLib/objectController.h
+++ src/gameLib/objectController.h
@@ -2,9 +2,15 @@
 #define OBJECT_CONTROLLER
 
 
+#include "worldObject.h"
 
 struct ObjectController{
-
+    void (*objectUpdate) (WorldObject* this);
+    void (*objectCollide) (WorldObject* this, WorldObject* other);
+    void (*objectDestroy) (WorldObject* this);
 };
+typedef struct ObjectController ObjectController ;
+
+ObjectController* ObjectControllerInit(); //TODO : implementation
 
 #endif
